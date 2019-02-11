52f0ea6cdbc69bac16f0e022ae71632da05a8a24
Gerardo Lima <glima@equalexperts.com>
2019-02-11 16:47:41 +0000

    Improve webpack.config
    When NODE_MODE value is
    - 'production': the file dist/bundle-analyser-report.html is generated;
    - else: the browser is launched with the client-side application

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv';
 import * as HardSourceWebpackPlugin from 'hard-source-webpack-plugin';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as path from 'path';
+import * as webpack from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 // TODO: check expected env variables
@@ -11,13 +12,40 @@ dotenv.config({path: path.join(__dirname, '.env')});
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const SERVER_PORT = process.env.PORT || '3000';
 const DEV_PORT = process.env.DEV_PORT || '8080';
+const PATH_DIST = path.join(__dirname, 'dist');
 
-module.exports = {
+
+// webpack plugins
+// ----------------------------------------------------------------------------
+const plugins: webpack.Plugin[] = [
+  new HardSourceWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    title: 'TypeScript and React',
+    favicon: './src/client/favicon.ico',
+    filename: 'index.html',
+    template: './src/client/index.ejs',
+  }),
+];
+
+if (IS_PRODUCTION) {
+  plugins.push(new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: false,
+    reportFilename: path.join(PATH_DIST, 'bundle-analyser-report.html'),
+  }));
+} else {
+  plugins.push(new CheckerPlugin());
+}
+
+
+// webpack main configuration object
+// ----------------------------------------------------------------------------
+const buildConfig: webpack.Configuration = {
   mode: IS_PRODUCTION ? 'production' : 'development',
-  devtool: IS_PRODUCTION ? '' : 'inline-source-map',
+  devtool: IS_PRODUCTION ? false : 'inline-source-map',
   entry: ['./src/client/client'],
   output: {
-    path: path.join(__dirname, 'dist', 'public'),
+    path: path.join(PATH_DIST, 'public'),
     filename: `[name]-[hash:8]-bundle.js`,
     publicPath: '/public/',
   },
@@ -25,7 +53,7 @@ module.exports = {
     watchContentBase: true,
     open: true,
     public: `http://localhost:${SERVER_PORT}`,
-    port: DEV_PORT
+    port: parseInt(DEV_PORT, 10)
   },
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
@@ -80,19 +108,11 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new CheckerPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'TypeScript and React',
-      favicon: './src/client/favicon.ico',
-      filename: 'index.html',
-      template: './src/client/index.ejs',
-    }),
-    new HardSourceWebpackPlugin(),
-    new BundleAnalyzerPlugin(),
-  ],
+  plugins,
   externals: {
      'react': 'React',
      'react-dom': 'ReactDOM',
   },
 };
+
+export default buildConfig;
